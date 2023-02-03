ed115d1b5fa578184a22cf8476bf95eb9f3456b8
KIBI312 <isken00.kg@gmail.com>
2023-02-03 16:48:10 +0600

    Adds security configuration

---
+++ src/main/java/com/seitov/news/security/AppUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.seitov.news.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.seitov.news.entity.User;
+import com.seitov.news.repository.UserRepository;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AppUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if(user.isEmpty()){
+            throw new UsernameNotFoundException("No user found with this username: " + username);
+        }
+        return user.get();
+    }
+    
+}
---
+++ src/main/java/com/seitov/news/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.seitov.news.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    private final AppUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(AppUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/management").hasRole("ADMIN"))
+            .formLogin((form) -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true).permitAll())
+            .logout((logout) -> logout
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID").permitAll());
+        return http.build();
+    }
+
+}
