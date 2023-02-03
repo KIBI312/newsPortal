29b14f830d3d48c1fb8a9c607aa8d66e7ec0afd4
KIBI312 <isken00.kg@gmail.com>
2023-02-03 17:56:08 +0600

    Adds auth related controller and login page

---
+++ src/main/java/com/seitov/news/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.seitov.news.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    
+    @GetMapping(path = "/login")
+    public String login() {
+        return "login";
+    }
+
+}
