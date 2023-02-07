c3c888c30c90203926d08f2dea151ede9f218218
KIBI312 <isken00.kg@gmail.com>
2023-02-07 19:18:55 +0600

    Adds DTO for registration process

---
+++ src/main/java/com/seitov/news/dto/RegistrationDto.java
@@ -0,0 +1,23 @@
+package com.seitov.news.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.seitov.news.validation.MatchingPasswords;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@MatchingPasswords(message = "Passwords are not matching")
+public class RegistrationDto {
+    
+    @NotNull
+    @Length(min = 4, max = 20, message = "Username length must be between 4 and 20 characters")
+    private String username;
+    @NotNull
+    @Length(min = 4, max = 20, message = "Password length must be between 8 and 20 characters")
+    private String password;
+    @NotNull
+    private String matchingPassword;
+
+}
