cd2e3c674f2653526fd817fbc4d6f50952abb395
KIBI312 <isken00.kg@gmail.com>
2023-02-10 17:23:31 +0600

    Adds different status code to POST-"/register" mapping

--- src/main/java/com/seitov/news/controller/AuthController.java
+++ src/main/java/com/seitov/news/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.seitov.news.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,7 @@ import com.seitov.news.exception.UserRegistrationException;
 import com.seitov.news.service.AuthService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @Controller
@@ -37,17 +39,22 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String registerAccount(@ModelAttribute("registrationDto") @Valid RegistrationDto registrationDto, BindingResult result, Model model,  HttpServletRequest request){
+    public String registerAccount(@ModelAttribute("registrationDto") @Valid RegistrationDto registrationDto, 
+                                BindingResult result, Model model,  HttpServletRequest request, 
+                                HttpServletResponse response) {
         if(result.hasErrors()){
+            response.setStatus(400);
             return "register";
         }
         try {
             authService.register(registrationDto);
         } catch (UserRegistrationException e) {
             model.addAttribute("error", e.getMessage());
+            response.setStatus(400);
             return "register";
         }
+        response.setStatus(201);
         return "login";
     }
-
+    
 }
