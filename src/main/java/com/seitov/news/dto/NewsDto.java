e5e2dd34398f256e79c168b254a2cbb4f36e944d
KIBI312 <isken00.kg@gmail.com>
2023-02-25 14:37:57 +0600

    Adds validation annotations for NewsDto

--- src/main/java/com/seitov/news/dto/NewsDto.java
+++ src/main/java/com/seitov/news/dto/NewsDto.java
@@ -3,7 +3,9 @@ package com.seitov.news.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -13,9 +15,17 @@ import java.util.UUID;
 public class NewsDto {
 
     private UUID articleId;
+
+    @NotNull
+    @Length(min = 5, max = 100, message = "Title length must be between 5 and 100 characters")
     private String title;
+    @NotNull
+    @Length(min = 5, max = 500, message = "Description length must be between 5 and 500 characters")
     private String description;
+    @NotNull
+    @Length(min = 5, max = 2048, message = "Content length must be between 5 and 2048 characters")
     private String content;
+    @NotNull
     private LocalDateTime publishedAt;
 
 }
